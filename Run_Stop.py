3f2592b2242189a50aa4bb709e496f7b19671dc2
LorenzoSiena <74120782+LorenzoSiena@users.noreply.github.com>
2022-04-11 16:37:40 +0200

    Add files via upload

---
+++ Run_Stop.py
@@ -0,0 +1,55 @@
+import configparser
+import sys
+
+#Se riceve 1 Start backup
+    #flag -> run
+#Se riceve 0 Stop Backup
+    #flag -> stop
+    #copy file sul desktop
+
+def run(config):
+    #CHANGE FLAG RUN (true)
+    config['STATE']['flag_run'] = '1'  # è messo in run
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)  # Salvo le modifiche
+    #TOAST NOTIFY(IL BACKUP E' RIPARTITO)
+    print("Backup Attivato!")
+       
+def save():
+    print("salvo")
+    print("salvo")
+    print("salvo")
+    print("salvo")
+    print("salvo")
+    #COPY FILE ON DESKTOP WITH TIMESTAMP
+    #DESKTOP->TIMESTAMP (60)
+    #DESKTOP->TIMESTAMP (40)
+    #DESKTOP->TIMESTAMP (20)
+    #DESKTOP->TIMESTAMP (TODAY)
+    print("salvo")
+
+def stop(config):
+    #CHANGE FLAG RUN (false)
+    config['STATE']['flag_run'] = '0'  # è messo in run
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)  # Salvo le modifiche
+    save()
+    print("Backup Disattivato")
+    #TOAST NOTIFY(FILE CREATI E BACKUP FERMO)
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("ERRORE: Argomenti mancanti")
+    config = configparser.ConfigParser()
+
+    if sys.argv[1] == 0:
+        stop(config)
+    elif sys.argv[1] == 1:
+        run(config)
+    else :
+        sys.exit("ERRORE: Argomenti sbagliati contattare admin")
+
+
+
+if __name__ == "__main__":
+    main()
