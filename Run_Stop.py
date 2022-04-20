663f6d2e8feef11ad7a7d172aa7f5d72dcb90150
LorenzoSiena <74120782+LorenzoSiena@users.noreply.github.com>
2022-04-20 15:54:40 +0200

    Update Run_Stop.py

--- Run_Stop.py
+++ Run_Stop.py
@@ -1,5 +1,10 @@
 import configparser
+import datetime
+import os
+import platform
+import shutil
 import sys
+from time import strftime
 
 #Se riceve 1 Start backup
     #flag -> run
@@ -14,17 +19,64 @@ def run(config):
         config.write(configfile)  # Salvo le modifiche
     #TOAST NOTIFY(IL BACKUP E' RIPARTITO)
     print("Backup Attivato!")
-       
-def save():
+
+
+def time_stamp_folder(src,path_desktop,bool):
+    #se il file di backup esiste
+    if os.path.exists(src) :
+
+            #data modifica in perch
+            data_mod = os.path.getmtime(src) 
+            
+            if bool:
+                #nome_cartella da salvare sul desktop
+                name_dir = datetime.fromtimestamp(data_mod).strftime("Backup_di_Oggi_%d%m") #fix
+            else:
+                 name_dir = datetime.fromtimestamp(data_mod).strftime("%H%M%m%d") #fix minuto/ora/giorno/mese
+            
+            #desktop+nomecartella_data
+            full_path=os.path.join(path_desktop,name_dir)
+            #crea la cartella
+            os.makedirs(full_path, exist_ok=True)
+            #copia il file sul path desktop
+            shutil.copy2(src, full_path)
+            print("File copiato correttamente")
+    else:   #exception
+        print(src," non è stato trovato")
+
+def save(config):
     print("salvo")
-    print("salvo")
-    print("salvo")
-    print("salvo")
-    print("salvo")
+    #leggo i path da config
+    config.read('config.ini')  ##ridondanza? 
+    dst1 = config['PATH']['dst1']
+    dst2 = config['PATH']['dst2']
+    dst3 = config['PATH']['dst3']
+    daily = config['PATH']['daily']
+    src = config['PATH']['src']
+    
+    full_dst1 = os.path.join(dst1,src)
+    full_dst2 = os.path.join(dst2,src)
+    full_dst3 = os.path.join(dst3,src)
+    full_daily = os.path.join(daily,src)
+
+    # creo una cartella dove il nome è la data dell'ultima modifica dei file 
     #COPY FILE ON DESKTOP WITH TIMESTAMP
-    #DESKTOP->TIMESTAMP (60)
+
+    if platform.system() == 'linux': 
+        path_desktop= os.path.join(os.path.join(os.path.expanduser('~')),'Scrivania')
+    else:
+        path_desktop= os.path.join(os.path.join(os.environ['USERPROFILE']),'Desktop')
+
+        #DESKTOP->TIMESTAMP (60)
+    
+    time_stamp_folder(full_dst1,path_desktop,False)
     #DESKTOP->TIMESTAMP (40)
+    time_stamp_folder(full_dst2,path_desktop,False)
     #DESKTOP->TIMESTAMP (20)
+    time_stamp_folder(full_dst3,path_desktop,False)
+    #daily-> Nome deve essere->Giornaliero  :/
+    time_stamp_folder(full_daily,path_desktop,True)
+    
     #DESKTOP->TIMESTAMP (TODAY)
     print("salvo")
 
@@ -34,11 +86,12 @@ def stop(config):
     config['STATE']['flag_run'] = 'stop'  # è messo in run
     with open('config.ini', 'w') as configfile:
         config.write(configfile)  # Salvo le modifiche
-    save()
+    save(config)
     print("Backup Disattivato")
     #TOAST NOTIFY(FILE CREATI E BACKUP FERMO)
 
 def main():
+
     if len(sys.argv) < 2:
         sys.exit("ERRORE: Argomenti mancanti")
     config = configparser.ConfigParser()
