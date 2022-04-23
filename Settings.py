a8f747ca9944e832cb97ff78899fe1f1d12965de
LorenzoSiena <74120782+LorenzoSiena@users.noreply.github.com>
2022-04-23 17:55:26 +0200

    Working Setting.py

--- Settings.py
+++ Settings.py
@@ -1,4 +1,4 @@
-#GUI= Imposta i path di lavoro
+# GUI= Imposta i path di lavoro
 
 from ast import If
 import configparser
@@ -6,88 +6,97 @@ import os
 import platform
 import PySimpleGUI as sg
 
+
 def gui():
     layout = [
-        [ # ---- Titolo
-            sg.Push(),sg.Text('Modifica il percorso del backup'),sg.Push()
-        ],[ # ---- LineaVuota
+        [  # ---- Titolo
+            sg.Push(), sg.Text('Modifica il percorso del backup'), sg.Push()
+        ], [  # ---- LineaVuota
             sg.Text('')
-        ], [ # ---- New row
-            sg.Text('File sorgente', size=(20, 1)), 
-            sg.InputText(key="src"), 
-            sg.FileBrowse(target="src", button_text = "Scegli") 
-        ], [ # ---- New row
-            sg.Text('1°dest(Ogni 20 minuti)', size=(20, 1)), 
-            sg.InputText(key="dst1"), 
-            sg.FolderBrowse(target="dst1",button_text = "Scegli") 
-        ], [ # ---- New row
-            sg.Text('2°dest(Ogni 40 minuti)', size=(20, 1)), 
-            sg.InputText(key="dst2"), 
-            sg.FolderBrowse(target="dst2", button_text = "Scegli") 
-        ], [ # ---- New row
-            sg.Text('3°dest(Ogni 60 minuti)', size=(20, 1)), 
-            sg.InputText(key="dst3"), 
-            sg.FolderBrowse(target="dst3", button_text = "Scegli") 
-        ], [ # ---- New row
-            sg.Text('Salvataggio Giornaliero', size=(20, 1)), 
-            sg.InputText(key="daily"), 
-            sg.FolderBrowse(target="daily", button_text = "Scegli") 
-        ],  [ # ----  Submit/Cancel
-            sg.Submit(button_color= "green",size=(10, 1), button_text = "Ok"),
-            sg.Cancel(button_color= "red", button_text = "Annulla")
+        ], [  # ---- New row
+            sg.Text('File sorgente', size=(20, 1)),
+            sg.InputText(key="src"),
+            sg.FileBrowse(target="src", button_text="Scegli")
+        ], [  # ---- New row
+            sg.Text('1°dest(Ogni 20 minuti)', size=(20, 1)),
+            sg.InputText(key="dst1"),
+            sg.FolderBrowse(target="dst1", button_text="Scegli")
+        ], [  # ---- New row
+            sg.Text('2°dest(Ogni 40 minuti)', size=(20, 1)),
+            sg.InputText(key="dst2"),
+            sg.FolderBrowse(target="dst2", button_text="Scegli")
+        ], [  # ---- New row
+            sg.Text('3°dest(Ogni 60 minuti)', size=(20, 1)),
+            sg.InputText(key="dst3"),
+            sg.FolderBrowse(target="dst3", button_text="Scegli")
+        ], [  # ---- New row
+            sg.Text('Salvataggio Giornaliero', size=(20, 1)),
+            sg.InputText(key="daily"),
+            sg.FolderBrowse(target="daily", button_text="Scegli")
+        ], [  # ----  Submit/Cancel
+            sg.Submit(button_color="green", size=(10, 1), button_text="Ok"),
+            sg.Cancel(button_color="red", button_text="Annulla")
         ]
     ]
     window = sg.Window('Windows Backup Manager').Layout(layout)
     while True:
-        event, values = window.Read() # Run the window until an "event" is triggered
+        event, values = window.Read()  # Run the window until an "event" is triggered
         if event == "Ok":
             return values
         elif event is None or event == "Annulla":
             return None
 
+
 def test_ini():
-    if platform.system() == 'Windows': 
-        config_path=os.path.join(os.path.expanduser('~\Documents'),'WinTaskBackManager\config.ini')
+    if platform.system() == 'Windows':
+        config_path = os.path.join(os.path.expanduser('~\Documents'), 'WinTaskBackManager\config.ini')
     else:
-        config_path = 'config.ini' #test su linux
+        config_path = 'config.ini'  # test su linux
     if not os.path.isfile(config_path):
-        result['src'] ="" #src
-        result['dst1'] ="" #dst1
-        result['dst2'] ="" #dst2
-        result['dst3'] ="" #dst3
-        result['daily'] ="" #daily
+        result = {
+            "src": "",
+            "dst1": "",
+            "dst2": "",
+            "dst3": "",
+            "daily": ""
+        }
         set_ini(result)
 
 
 def set_ini(result):
-    src = result['src'] #src
-    dst1 = result['dst1'] #dst1
-    dst2 =result['dst2']  #dst2
-    dst3 =result['dst3']  #dst3
-    daily =result['daily']  #daily
+    src = result['src']  # src
+    dst1 = result['dst1']  # dst1
+    dst2 = result['dst2']  # dst2
+    dst3 = result['dst3']  # dst3
+    daily = result['daily']  # daily
 
-    if platform.system() == 'Windows': 
-        config_path=os.path.join(os.path.expanduser('~\Documents'),'WinTaskBackManager\config.ini')
+    if platform.system() == 'Windows':
+        config_path = os.path.join(os.path.expanduser('~\Documents'), 'WinTaskBackManager\config.ini')
     else:
-        config_path = 'config.ini' #test su linux
-    
+        config_path = 'config.ini'  # test su linux
+
     config = configparser.ConfigParser()
-    state = config['STATE']['st'] 
-    flg = config['STATE']['flag_run'] 
-    
-    config['STATE'] ={
-                    'st' : state,
-                    'flag_run' : flg
-                    }
+    try:
+        state = config['STATE']['st']
+        flg = config['STATE']['flag_run']
+    except KeyError:
+        state = 'a'
+        flg = 'run'
+
+    config['STATE'] = {
+        'st': state,
+        'flag_run': flg
+    }
     config['PATH'] = {
-                    'dst1': dst1,  
-                    'dst2': dst2, 
-                    'dst3': dst3,  
-                    'daily': daily,  
-                    'src': src    
-                    }
-    with open(config_path, 'w') as configfile: 
+        'dst1': dst1,
+        'dst2': dst2,
+        'dst3': dst3,
+        'daily': daily,
+        'src': src
+    }
+    with open(config_path, 'w') as configfile:
         config.write(configfile)
+
 
 if __name__ == "__main__":
     test_ini()
